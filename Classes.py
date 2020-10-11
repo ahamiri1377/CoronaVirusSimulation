8fa03fee3ad7db6dd8aa0d1e13f385b91e7173e3
Amir Hossein Amiri <ah.amiri1377@gmail.com>
2020-10-11 10:52:22 +0330

    Add files via upload
    hierarchical and distributed model

--- Classes.py
+++ Classes.py
@@ -3,7 +3,8 @@ import numpy as np
 class Group():
     def __init__(self, depth, heal_prop, infection_prob, id):
         self.depth = depth
-        self.S = np.random.uniform(0, 1, 1)
+        q = np.random.binomial(1, 0.9)
+        self.S = q * np.random.uniform(0.8, 1, 1)[0] + (1 - q) * np.random.uniform(0, 0.2, 1)[0]
         self.next_S = self.S
         self.I = 1 - self.S
         self.next_I = self.I
@@ -19,7 +20,7 @@ class Group():
     def state(self):
         if self.S >= self.I and self.S >= self.R:
             return 'S'
-        elif self.I >= self.S and self.I >= self.S:
+        elif self.I >= self.S and self.I >= self.R:
             return 'I'
         else:
             return 'R'
@@ -29,34 +30,49 @@ class Group():
             self.child.append(Group(self.depth - 1, self.heal_prob, self.infection_prob, i))
 
     def make_childs_in_touch(self):
-        for c in self.child:
-            for c_prime in self.child:
-                if c != c_prime:
-                    c.in_touch_groups.append(c_prime)
+        if self.depth:
+            for c in self.child:
+                for c_prime in self.child:
+                    if c != c_prime:
+                        c.in_touch_groups.append(c_prime)
 
     def total_recovered(self):
         if self.depth:
-            con = 0
+            con, cont = 0, 0
             for c in self.child:
-                con += c.total_recovered()
-            return con
+                n, m = c.total_recovered()
+                con += n
+                cont += m
+            return con, cont
         else:
             if self.state() == 'R':
-                return 1
+                return 1, self.R
             else:
-                return 0
-    
+                return 0, self.R
+
     def total_infected(self):
         if self.depth:
-            con = 0
+            con, cont = 0, 0
             for c in self.child:
-                con += c.total_infected()
-            return con
+                n, m = c.total_infected()
+                con += n
+                cont += m
+            return con, cont
         else:
             if self.state() == 'I':
-                return 1
+                return 1, self.I
             else:
-                return 0
+                return 0, self.I
+    
+    def size(self):
+        if self.depth == 1:
+            return len(self.child)
+        elif self.depth > 1:
+            con = 0
+            for c in self.child:
+                con += c.size()
+            return con
+
 
     def update(self):
         self.S = self.next_S
@@ -68,46 +84,45 @@ class Group():
 
     def begin(self):
         if self.depth:
-            self.make_childs(np.random.poisson(10))
+            self.make_childs(np.random.poisson(30))
             self.make_childs_in_touch()
             for c in self.child:
                 c.begin()
     
-    def pass_time(self, parent_infected):
+    def pass_time(self, parent_S, parent_I, parent_R):
+        if self.depth:
+            count = 0
+            for c in self.child:
+                c.pass_time(self.S, self.I, self.R)
+                if c.state() == 'I':
+                    count += 1
+            if not self.state() == 'R':
+                if count / len(self.child) > self.I:
+                    self.next_I = (count / len(self.child) + self.I) / 2
+                    self.next_S = self.S - (self.next_I - self.I)
+
+        if self.I < parent_I:
+            if np.random.binomial(1, self.infection_prob):
+                    self.next_S = (self.S + parent_S) / 2
+                    self.next_I = (self.I + parent_I) / 2
+                    self.next_R = (self.R + parent_R) / 2
+
+        if not self.state() =='R':
+            for i in self.in_touch_groups:
+                if self.I < i.I:
+                    if np.random.binomial(1, self.infection_prob):
+                        self.next_S = (self.S + i.S) / 2
+                        self.next_I = (i.I + self.I) / 2
+                        self.next_R = (self.R + i.R) / 2
         if self.state() == 'I':
             if np.random.binomial(1, self.heal_prob):
                 self.next_S = self.S / 2
                 self.next_I = self.I / 2
-                self.next_R = (self.R + 1) / 2
-        elif self.state() == 'R':
+                self.next_R = 1 - self.next_I - self.next_S
+        if self.state() == 'R':
             if np.random.binomial(1, self.heal_prob):
                 self.next_S = self.S / 2
                 self.next_I = self.I / 2
                 self.next_R = (self.R + 1) / 2
-        else:
-            for i in self.in_touch_groups:
-                if i.state() == 'I':
-                    if np.random.binomial(1, self.infection_prob):
-                        self.next_S = (self.S + i.S) / 2
-                        self.next_I = (self.I + i.I) / 2
-                        self.next_R = (self.R + i.R) / 2
-        if parent_infected == 'I':
-            if not self.state() == 'R':
-                if np.random.binomial(1, self.infection_prob):
-                    self.next_S = self.S / 2
-                    self.next_I = (self.I + 1) / 2
-                    self.next_R = self.R / 2
-        if self.depth:
-            count = 0
-            for c in self.child:
-                c.pass_time(self.state())
-                if c.state() == 'I':
-                    count += 1
-            if count >= len(self.child) / 2:
-                self.next_S = self.S / 2
-                self.next_I = (self.I + 1) / 2
-                self.next_R = self.R / 2
-
-    
-
-    
+        
+        
--- SIR.py
+++ SIR.py
@@ -1,48 +1,48 @@
 from Classes import Group
 from matplotlib import pyplot as plt
 
-def check(pi, pr, city_number, iter):
-    cities = []
-    for i in range(city_number):
-        c = City(pi, pr, i)
-        c.begin()
-        cities.append(c)
+# def check(pi, pr, city_number, iter):
+#     cities = []
+#     for i in range(city_number):
+#         c = City(pi, pr, i)
+#         c.begin()
+#         cities.append(c)
 
-    #  make_country_in_touch
-    for city in cities:
-        for in_touch_city in cities:
-            if city != in_touch_city:
-                city.in_touch_cities.append(in_touch_city)
+#     #  make_country_in_touch
+#     for city in cities:
+#         for in_touch_city in cities:
+#             if city != in_touch_city:
+#                 city.in_touch_cities.append(in_touch_city)
 
-    s = 0
-    for q in cities:
-        s += q.size()
-    print(s)
+#     s = 0
+#     for q in cities:
+#         s += q.size()
+#     print(s)
 
-    it = []
-    numb = []
-    num = []
+#     it = []
+#     numb = []
+#     num = []
 
-    for i in range(iter):
-        for c in cities:
-            c.pass_time()
-        con ,conn = 0, 0
-        for cc in cities:
-            cc.update()
-            con += cc.total_recovered()
-            conn += cc.total_infection()
+#     for i in range(iter):
+#         for c in cities:
+#             c.pass_time()
+#         con ,conn = 0, 0
+#         for cc in cities:
+#             cc.update()
+#             con += cc.total_recovered()
+#             conn += cc.total_infection()
         
-    if con >= 0.9 * s:
-        return True
-    else:
-        return False
+#     if con >= 0.9 * s:
+#         return True
+#     else:
+#         return False
 
         
 
 
-city_number = 10
-tup1 = []
-tup2 = []
+# city_number = 10
+# tup1 = []
+# tup2 = []
 # for i in range(40):
 #     beg = 0
 #     end = 100
@@ -56,46 +56,60 @@ tup2 = []
 #     tup2.append(end / 100)
     
 
-# plt.plot(tup1, tup2, '-')
-# plt.title('P_I vs P_R')
-# plt.xlabel('P_I')
-# plt.ylabel('P_R')
-# # plt.text(max(tup1) / 10, max(tup2) / 2, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr))
-# plt.show()
+infec_i = []
+infec_t = []
+rec_i = []
+rec_t = []
 
-# # plt.plot(it, numb, '.')
-# # plt.title('Recovery')
-# # plt.xlabel('time')
-# # plt.ylabel('recovered ratio')
-# # plt.text(800, max(numb) / 2, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr))
-# # plt.show()
-
-# # plt.plot(it, num, '.')
-# plt.title('Infection')
-# plt.xlabel('time')
-# plt.ylabel('infected ratio')
-# plt.text(800, max(num) / 2, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr))
-
-# plt.show()
-t = []
-infec = []
-rec = []
-g = Group(4, 0.02, 0.02, 1)
+depth = int(input())
+pr = float(input())
+pi = float(input())
+g = Group(depth, pr, pi, 1)
 g.begin()
-for i in range(1000):
+size = g.size()
+for i in range(2000):
     print(i + 1)
-    g.pass_time('S')
+    g.pass_time(1, 0, 0)
     g.update()
     t.append(i + 1)
-    infec.append(g.total_infected())
-    rec.append(g.total_recovered())
+    n, m = g.total_infected()
+    infec_i.append(n / size)
+    infec_t.append(m / size)
+    n, m = g.total_recovered()
+    rec_i.append(n / size)
+    rec_t.append(m / size)
+    
+    
 
-plt.plot(t, infec, '.')
-plt.title('Infected')
+plt.plot(t, infec_i, '-', label='infected')
+plt.plot(t, rec_i, '-', label='recovered')
+plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0.)
+plt.title('Infected and Recovered')
 plt.xlabel('time')
-plt.ylabel('infected ratio')
-# plt.text(800, max(numb) / 2, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr))
+plt.ylabel('ratio')
+plt.text(1500, 0.5, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr) + r'$\ \  Depth:  $' + str(depth) + r'$\ \  Size:  $' + str(size))
 plt.show()
+
+plt.plot(t, infec_t, '-', label='infected')
+plt.plot(t, rec_t, '-', label='recovered')
+plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0.)
+plt.title('Infected and Recovered')
+plt.xlabel('time')
+plt.ylabel('ratio')
+plt.text(1500, 0.5, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr) + r'$\ \  Depth:  $' + str(depth) + r'$\ \  Size:  $' + str(size))
+plt.show()
+
+plt.plot(t, infec_i, '-', label='infected-1')
+plt.plot(t, rec_i, '-', label='recovered-1')
+plt.plot(t, infec_t, '-', label='infected-2')
+plt.plot(t, rec_t, '-', label='recovered-2')
+plt.legend(bbox_to_anchor=(1.01, 1), loc='upper left', borderaxespad=0.)
+plt.title('Infected and Recovered')
+plt.xlabel('time')
+plt.ylabel('ratio')
+plt.text(1500, 0.5, r'$P_I: $' + str(pi) +r'$\ \  P_R:  $' + str(pr) + r'$\ \  Depth:  $' + str(depth) + r'$\ \  Size:  $' + str(size))
+plt.show()
+
 
 
 
